b3eb648a24e5aa0994e271f17e3cb02cecd55702
S Chiang <63751262+gosaurus@users.noreply.github.com>
2025-03-03 14:29:30 +0000

    Added conversion of bus arrival time to minutes and sorting by arrival time

--- src/Arrival.cs
+++ src/Arrival.cs
@@ -6,9 +6,24 @@ namespace BusBoard {
         public required string lineId { get; set; }
         public required string destinationName { get; set; }
         public required int timeToStation { get; set; }
+        public int? minutesToStation { 
+            get {
+                return convertSecondsToMinutes(timeToStation); 
+            }
+        
+        }
+        public int convertSecondsToMinutes(int timeToStation) {
+            var timeToStationMinutes = timeToStation / 60;
+            return timeToStationMinutes;
+        }
         public override string ToString()
         {
-            return $"Bus {lineId} towards {destinationName} arriving in {timeToStation} mins.";
+            return $"Bus {lineId} towards {destinationName} arriving in {convertSecondsToMinutes(timeToStation)} mins.";
         } 
+
+        public static List<Arrival> SortListOfArrivals(List<Arrival> arrivalsList)
+        {
+            return arrivalsList.OrderBy(arrival => arrival.timeToStation).ToList();
+        }
     }
 }
